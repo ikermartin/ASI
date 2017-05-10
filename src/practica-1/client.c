68a90309d6f7bef8190493ddf1ae1f6bdb5d1722
ikermartin <ikertxin@gmail.com>
2017-05-10 19:34:09 +0200

    Programa cliente de la práctica 1 completo
    Responde adecuadamente a los seis secretos. 
    Puntos de mejora:
    - Sustituír los getchar()
    - Introducir un menú
    - Ajustar los tiempos de espera de los secretos 4 y 5.

--- src/practica-1/client.c
+++ src/practica-1/client.c
@@ -1,57 +1,168 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * File:   client.c
+ * Author: docencia
+ *
+ * Created on March 8, 2017, 8:29 AM
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <stdio_ext.h>
+#include <unistd.h>
+#include <sys/types.h> //waitpid
+#include <sys/wait.h> //waitpid
+#include <pthread.h> //threads
 
-#include "client.h"		// Include propio en directorio actual
 
-#define MIN_PARAMS 2		// EL nombre de programa es un parametro
-#define MAX_PARAMS 2		// Incluido el nombre de programa
 
-/********************* PROGRAMA DE LA PRACTICA X **************************/
-/* Historia:
+/*
+ *
+ */
 
-28/02/2017	AFV	Inicio codificación
+int pidMonitor, threads;
+int seinales=0;
 
-*/
 
-/*------------------------- Mensaje de información -----------------------*/
+void echo();
+void creaHilo();
+void *func_hilo(void *arg);
+void cuentaSeinales();
+void echoCount();
 
-void print_usage(char *command)
-{
-	printf("El formato del comando es:\n");
-	printf("%s <id proceso MONITOR>\n",command);
+
+
+
+int main(int argc, char** argv) {
+    int pidhijo1,pidhijo2, status;
+    
+    int hilos;
+    
+    pidMonitor=atoi(argv[1]);
+    printf("PidMonitor= %d \n", pidMonitor);
+    
+    signal(SIGUSR2,echo);
+    
+    
+    printf("Padre con PID: %d\n",getpid());
+
+    printf("Pulsar cualquier tecla para ejecutar el ejercicio 1\n");
+    getchar();
+    kill(pidMonitor,SIGUSR1);
+    sleep(5);
+
+
+    printf("Pulsar cualquier tecla para ejecutar el ejercicio 2\n");
+    getchar();
+    kill(pidMonitor,SIGUSR1);
+    sleep(5);
+
+    printf("Pulsar cualquier tecla para ejecutar el ejercicio 3\n");
+    getchar();
+    kill(pidMonitor,SIGUSR1);
+    sleep(1);    
+    
+    if((pidhijo1=fork())==0) // código hijo 1
+    {
+        printf("\tHijo1 con PID: %d\n",getpid());
+        signal(SIGUSR2, creaHilo);
+	     
+        while(1)
+        {
+            pause();
+        }
+    }
+    
+    else if((pidhijo2=fork())==0) // código hijo 2
+    {
+        printf("\tHijo2 con PID: %d\n",getpid());
+		signal(SIGUSR1, cuentaSeinales);
+		signal(SIGUSR2, echoCount);
+        
+        while(1)
+        {
+            pause();
+        }
+    }
+    
+    else    //código padre
+    {     
+      
+        kill(pidMonitor,SIGUSR1);
+        sleep(5);
+
+		printf("Pulsar cualquier tecla para ejecutar el ejercicio 4\n");
+		getchar();
+		sleep(15);
+
+		printf("Pulsar cualquier tecla para ejecutar el ejercicio 5\n");
+		getchar();
+		kill(pidMonitor,SIGUSR1);
+		sleep(30);
+        
+        printf("Pulsar cualquier tecla para ejecutar el ejercicio 6\n");
+        getchar();
+		kill(pidMonitor,SIGUSR1);
+		sleep(1);
+	 
+	 	//matamos a los hijos antes de finalizar
+
+		kill(pidhijo1,SIGINT);
+        waitpid(pidhijo1,&status,0);
+        
+        kill(pidhijo2,SIGINT);
+        waitpid(pidhijo2,&status,0);
+ 
+        exit(1);
+    }
+    
+    return (EXIT_SUCCESS);
 }
 
-/*------------------------------------------------------------------------*/
-
-/*--------------------- Imprimir parametros ------------------------------*/
-
-void print_parameters(int argc, char *argv[])
+void echo()
 {
-int i;
-	for(i=0;i<argc;i++)
-		printf("PAR %d: <%s>\n",i,argv[i]);
+    kill(pidMonitor,SIGUSR2);
+    printf("\tSIGUSR2 recibida y eco enviado\n");
 }
 
-/*------------------------------------------------------------------------*/
-
-/*---------------------- Función principal -------------------------------*/
-/* Comentario:
-Conviene que esta función sea fácil de leer y de una idea de la forma de funcionar la lógica del programa. Apóyate en funciones auxiliares para conseguir este objetivo.
-*/
-
-int main(int argc, char *argv[])
+void creaHilo()
 {
-	if(argc < MIN_PARAMS || argc > MAX_PARAMS) {
-		fprintf(stderr,"Mal formato de comando.\n");
-		print_usage(argv[0]);
-		exit(1);
-	}
-	print_parameters(argc,argv);
-	exit(0);
+    pthread_t thd;
+    pthread_create(&thd,NULL,(void*)func_hilo,NULL);
 }
 
-/*------------------------------------------------------------------------*/
+void *func_hilo(void *arg)
+{
+    int t=0;
+    threads++;
+    
+    printf("\tNuevo hilo creado. Actualmente hay: %d \n", threads);
+    
+    do{
+        if(t%5==0)
+        {
+            printf("\t\tAquí un thread que sigue vivo\n");
+        }
+        sleep(1);
+        t++;
+    }while(t<=10);
+}
 
-/**************************************************************************/
+void cuentaSeinales()
+{
+	printf("\tSeñal SIGUSR1 recibida\n");
+}
+
+void echoCount()
+{
+	seinales++;    
+	kill(pidMonitor,SIGUSR2);
+    printf("\tSIGUSR2 recibida y eco enviado. Van %d señales\n", seinales);
+}
 
